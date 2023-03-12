04eff5dcf276831c373526e0cf1b6e3b9ef1edec
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2023-03-12 09:23:38 +0900

    問題作成

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 import "./style.scss";
+
+function createQuestion() {
+  // 問題作成
+  const firstNumber = 100;
+  const secondNumber = 100;
+  const calcSign = "+";
+  return {
+    first: firstNumber,
+    second: secondNumber,
+    sign: calcSign,
+  };
+}
+
+const question = createQuestion();
+console.log(question);
